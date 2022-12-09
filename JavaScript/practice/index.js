2b67875f60e8b2f3e7c987cb6b9d1f5e6703b6e7
Anusha VS <anusha.vs@vonnue.com>
2022-12-09 09:59:11 +0530

    feat: practice section- javascript

---
+++ JavaScript/practice/index.js
@@ -0,0 +1,104 @@
+// let myAge = 22
+// let humanDogRatio = 7
+// let count = myAge * humanDogRatio
+// let myDogAge = count
+// console.log(myDogAge)
+
+// let bonusPoints = 50
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+// let count = 0
+// function save(){
+//     console.log("The button was clicked")
+// }
+
+// function startRace(){
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+//     console.log(0)
+// }
+// startRace()
+
+// let a = 39
+// let b = 43
+// let c = 28
+// function logsOutSum(){
+//     let sum = a + b + c
+//     console.log(sum)
+// }
+// logsOutSum()
+
+// let a = 0
+// function laps(){
+//     a = a + 1
+//     console.log(a)
+// }
+// laps()
+// laps()
+// laps()
+// laps()
+
+// var a;
+// var b = 2;
+// console.log(b) 
+// a = 7;
+// b = a;
+// console.log(a)
+
+// var a = 5;
+// var b = 38;
+// var c = "It is a ";
+// a = a +1 ;
+// b = b + 3 ;
+// c = c + "string";
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// var a;
+// a = 5 % 2;
+// console.log(a)
+
+// var a = "\"first\"\n\t second \n third"
+// console.log(a)
+ 
+/////// 08/12/12 //////
+
+// var firstNameLength = 0
+// var firstName = "Herald"
+// firstNameLength = firstName.length
+// firstLetter = firstName[0]
+// lastLetter = firstName[firstName.length - 1]
+// console.log(firstNameLength)
+// console.log(firstLetter)
+// console.log(lastLetter)
+
+// var a = "Aello";
+// a = "Hello";
+// console.log(a)
+
+// function functionWithArgs(a, b){
+//     console.log(a - b)
+// }
+// functionWithArgs (10 , 5)
+// function functionArgsAddition(a, b){
+//     console.log ( a + b)
+// }
+// functionArgsAddition( 100, 30)
+
+// function date(){
+//     document.getElementById('display').innerHTML = Date()
+// }
+
+// function clickclick(){
+//     document.getElementById('click_me').innerHTML = "Booooooom!"
+// }
