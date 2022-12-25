20bea82022964432a11dc0bf45bbf77273d7b1a9
Anusha VS <anusha.vs@vonnue.com>
2022-12-25 20:15:48 +0530

    feat: udemi javascript udemy 187

--- JavaScript/section-13/script.js
+++ JavaScript/section-13/script.js
@@ -27,7 +27,7 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-// Selecting elements
+/*// Selecting elements
 console.log(document.documentElement)
 console.log(document.head)
 console.log(document.body)
@@ -102,4 +102,15 @@ logo.classList.remove("c", "j")
 logo.classList.toggle("c")
 logo.classList.contains("c")
 
-logo.className = "Jonas"
+// logo.className = "Jonas"*/
+
+const btnScrollTo = document.querySelector(".btn--scroll-to")
+const section1 = document.querySelector("#section--1")
+
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect()
+  console.log(s1coords)
+  console.log(e.target.getBoundingClientRect())
+  console.log("Current scroll(X/Y)", window.pageXOffset, pageYOffset)
+  console.log("height/width viewport", document.documentElement.clientHeight, document.documentElement.clientWidth)
+})
