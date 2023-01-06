becac03c2d54f1850cb2dbdf1f09d053d3679023
Anusha VS <anusha.vs@vonnue.com>
2023-01-06 21:49:20 +0530

    feat: 50 projects section 19

--- 50-projects/section_14-Random-Choice-Picker/script.js
+++ 50-projects/section_14-Random-Choice-Picker/script.js
@@ -5,6 +5,13 @@ textarea.focus();
 
 textarea.addEventListener("keyup", (e) => {
     createTags(e.target.value)
+
+    if (e.key === "Enter") {
+        setTimeout(() => {
+            e.target.value = ""
+        }, 10)
+        randomSelect()
+    }
 })
 function createTags(input) {
     // console.log(input)
@@ -12,8 +19,47 @@ function createTags(input) {
         .filter(tag => tag.trim() !== "")
         .map(tag => tag.trim())
 
-        tagsEl.innerHTML = "";
-        
-        tags.forEach()
+    tagsEl.innerHTML = "";
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement("span")
+        tagEl.classList.add("tag")
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl)
+    })
     // console.log(tags);
+}
+function randomSelect() {
+    // console.log(123);
+    const times = 30
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        }, 100)
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval)
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+            highlightTag(randomTag)
+        }, 100)
+    }, times * 100)
+    
+}
+function pickRandomTag() {
+    const tags = document.querySelectorAll(".tag")
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add("highlight")
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove("highlight")
 }
