4961d878af378ae3bd074baa46b2be41f40f817a
Anusha VS <anusha.vs@vonnue.com>
2023-01-10 20:03:05 +0530

    feat:javascript project 49

--- 50-projects/section_37-HoverBoard/script.js
+++ 50-projects/section_37-HoverBoard/script.js
@@ -19,7 +19,7 @@ function setColor(element){
     
 }
 function removeColor(element){
-    element.style.background = '#1d1d1d1'
+    element.style.background = '#1d1d1d'
     element.style.boxShadow = '0 0 2px #000'
 }
 function getRandomColor(){
